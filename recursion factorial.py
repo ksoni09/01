9c88d2c64669092a8c155cb7fbfaff694279ed1c
ksoni09 <157550682+ksoni09@users.noreply.github.com>
2024-02-24 21:59:10 -0500

    Add files via upload

---
+++ recursion factorial.py
@@ -0,0 +1,16 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def main():
+    number = int(input("Enter a non-negative integer: "))
+    
+    if number < 0:
+        print("Factorial is not defined for negative numbers.")
+    else:
+        result = factorial(number)
+        print("The factorial of", number, "is", result)
+
+main()
